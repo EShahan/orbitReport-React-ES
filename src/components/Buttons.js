d146bceccd04d363ecd3c76f00e72d6bdfcf4fd3
unknown <ctibalt@gmail.com>
2023-08-08 07:33:36 -0500

    Finished parts 3 and 4, possibly

--- src/components/Buttons.js
+++ src/components/Buttons.js
@@ -1,8 +1,20 @@
+import satData from "./satData";
+
 const Buttons = (props) => {
+  const filterByType = props.filterByType;
+  const setSat = props.setSat;
+  const displaySats = props.displaySats;
+
   return (
     <div>
-      <button>Placeholder Button</button>
-      <button>All Orbits</button>
+      {displaySats.map((sat, id) => {
+        return (
+          <button onClick={() => filterByType(sat)} key={id}>
+            {sat} Orbit
+          </button>
+        );
+      })}
+      <button onClick={() => setSat(satData)}>All Orbits</button>
       </div>
   );
 };
