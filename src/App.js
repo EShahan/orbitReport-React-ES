959ec889024f891e30474cb3ec68be7fdd0d937b
unknown <ctibalt@gmail.com>
2023-08-08 07:18:31 -0500

    Finished parts 1 and 2

--- src/App.js
+++ src/App.js
@@ -1,10 +1,13 @@
 import Buttons from "./components/Buttons";
 import Table from "./components/Table";
-
+import Banner from "./components/Banner";
+import satData from "./components/satData";
+import useState from "react";
 
 function App() {
   return (
     <div>
+      <Banner />
       <Buttons />
       <Table />
     </div>
--- src/components/Buttons.js
+++ src/components/Buttons.js
@@ -1,4 +1,4 @@
-const Buttons = () => {
+const Buttons = (props) => {
   return (
     <div>
       <button>Placeholder Button</button>
